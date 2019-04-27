788b3035080d2ade9c52f06d8af2a1e526d4c23a
Rongsong Shen <rshen@shenrs.eu>
2019-04-27 17:11:15 +0800

    easy-emacs

--- core/core-modules.el
+++ core/core-modules.el
@@ -1,5 +1,12 @@
+(defmacro module! (name docstring pkg-info features)
+  t)
+
+(defun load-module-definition (module)
+  (message "loading module information from %s..." module))
 
 (defun load-modules (dir)
-  t)
+  (let ((modules (directory-files-recursively dir "modules.el")))
+    (cl-loop for module in modules
+	     do (load-module-definition module))))
 
 (provide 'core-modules)
