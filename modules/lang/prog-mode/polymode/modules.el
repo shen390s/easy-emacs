0b3efa2a130a35da7f7de46f0287b0f9b9d66ec2
Rongsong Shen <shenrs@cn.ibm.com>
2019-08-09 10:58:57 +0800

    polymode

---
+++ modules/lang/prog-mode/polymode/modules.el
@@ -0,0 +1,22 @@
+(scope! poly-mode
+	prog-lang
+	polymode)
+
+(package! polymode
+	  "Extensible framework for multiple major modes"
+	  polymode)
+
+(package! poly-markdown
+	  "polymode for markdown mode"
+	  poly-markdown)
+
+(defun activate-poly-markdown ()
+  (require 'poly-markdown)
+  (poly-markdown-mode 1))
+
+(feature! poly-markdown
+	  "polymode for markdown mode"
+	  (polymode poly-markdown markdown)
+	  nil
+	  activate-poly-markdown
+	  nil)
