3c1d7489fe2fea7e78523a6cda81056f7e9283e4
Rongsong Shen <rshen@shenrs.eu>
2019-12-21 09:42:33 +0800

    update

--- modules/custom/modules.el
+++ modules/custom/modules.el
@@ -1,8 +1,10 @@
 (defun config-load-custom ()
   t)
 
-(defun enable-load-custom ()
-  (load custom-file t t))
+(defun enable-load-custom (theme)
+  (load custom-file t t)
+  (when theme
+      (load-theme theme)))
 
 (feature! load-custom
 	  "load customization file"
