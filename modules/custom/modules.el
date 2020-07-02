c89c0ae83747c4454330f20694c5d4a41161ed9a
Rongsong Shen <shenrs@cn.ibm.com>
2020-07-02 14:59:06 +0800

    tune adoc faces

--- modules/custom/modules.el
+++ modules/custom/modules.el
@@ -1,10 +1,27 @@
 (defun config-load-custom ()
   t)
 
+(defun gen-theme-list (theme)
+  (let ((name (symbol-name theme)))
+    (list (intern (format "%s-x%d"
+     		  name (x-display-pixel-width)))
+	  theme)))
+
+(defun load-theme-ex (theme flag)
+  (INFO! "Loading theme %s" theme)
+  (if (custom-theme-p theme)
+      (load-theme theme flag)
+    nil))
+
+(defun load-themes (themes flag)
+  (when themes
+    (unless (load-theme-ex (car themes) flag)
+      (load-themes (cdr themes) flag))))
+
 (defun enable-load-custom (&optional theme)
   (load custom-file t t)
   (when theme
-      (load-theme theme t)))
+      (load-themes (gen-theme-list theme) t)))
 
 (feature! load-custom
 	  "load customization file"
--- modules/lang/prog-mode/adoc/modules.el
+++ modules/lang/prog-mode/adoc/modules.el
@@ -8,18 +8,19 @@
 
 (defun adoc-tune-faces ()
   (progn
-    (cl-loop for face in '(adoc-align adoc-anchor adoc-generic
-				      adoc-monospace adoc-strong
-				      adoc-emphasis adoc-superscript
-				      adoc-subscript
-				      adoc-secondary-text
-				      adoc-replacement
-				      adoc-complex-replacement
-				      adoc-list-item adoc-table-del
-				      adoc-reference adoc-delimiter
-				      adoc-hide-delimiter adoc-comment
-				      adoc-warning adoc-preprocessor)
-	     do (set-face-attribute face nil :height 180)))
+    (let ((face-height (/ (x-display-pixel-width) 16)))
+      (cl-loop for face in '(adoc-align adoc-anchor adoc-generic
+					adoc-monospace adoc-strong
+					adoc-emphasis adoc-superscript
+					adoc-subscript
+					adoc-secondary-text
+					adoc-replacement
+					adoc-complex-replacement
+					adoc-list-item adoc-table-del
+					adoc-reference adoc-delimiter
+					adoc-hide-delimiter adoc-comment
+					adoc-warning adoc-preprocessor)
+	       do (set-face-attribute face nil :height face-height))))
   t)
 
 (defun ascii-doctor-setup ()
