4566175130beb7c16bc683bfbbded3fd4e7a4963
Rongsong Shen <shenrs@cn.ibm.com>
2020-07-22 11:16:41 +0800

    setup workenv

--- modules/app/preview/livemarkup/modules.el
+++ modules/app/preview/livemarkup/modules.el
@@ -2,14 +2,16 @@
 	  :docstring "live preview for org and asciidoc"
 	  :pkginfo (emacs-livemarkup :type git
 			             :host github
-			             :repo "dawsers/emacs-livemarkup"))
+			             :repo "shen390s/emacs-livemarkup"))
 
 (defun livemarkup-config ()
   (setq livemarkup-output-directory "/tmp"
-	livemarkup-close-buffer-delete-temp-file t)
+	livemarkup-close-buffer-delete-temp-file t
+	livemarkup-refresh-interval 3)
   t)
 
 (defun activate-livemarkup ()
+  (require 'livemarkup)
   (let ((file-name (buffer-file-name)))
     (let ((ext-name (file-name-extension file-name)))
       (cond
