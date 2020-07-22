607f49eb783f3b1d0c3c7afdf4d2ca9babb26af1
Rongsong Shen <shenrs@cn.ibm.com>
2020-07-22 11:58:37 +0800

    setup coding system

--- modules/settings/modules.el
+++ modules/settings/modules.el
@@ -1,8 +1,27 @@
 (defvar use-polymode nil
   "enable poly mode")
 
+(defvar my-coding-system 'utf-8
+  "coding system setting")
+
+(defun setup-coding-system (cs)
+  (prefer-coding-system cs)
+  (set-default-coding-systems cs)
+  (set-terminal-coding-system cs)
+  (set-keyboard-coding-system cs)
+  (if (boundp 'buffer-file-coding-system)
+      (setq-default buffer-file-coding-system cs)
+    (setq default-buffer-file-coding-system cs))
+
+  ;; Treat clipboard input as UTF-8 string first; compound text next, etc.
+  (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT
+					    STRING)))
+
 (defmacro activate-settings (&rest vars)
-  `(set-vars ,@vars))
+  `(progn
+     (set-vars ,@vars)
+     (setup-coding-system my-coding-system)))
+  
 
 (feature! settings
 	  "setting of settingsment"
