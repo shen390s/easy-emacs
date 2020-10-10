8fb8f9ab0c71c681d0d75593d36427d39f48cf25
Rongsong Shen <shenrs@cn.ibm.com>
2020-10-10 12:36:49 +0800

    yaml mode

---
+++ modules/lang/prog-mode/yaml/modules.el
@@ -0,0 +1,25 @@
+(scope! yaml prog-lang)
+
+(package! :name yaml-mode
+	  :docstring "A major Emacs mode for edit yaml document"
+	  :pkginfo (yaml-mode :type git
+			      :host github
+			      :repo "yaml/yaml-mode"))
+
+(defun config-yaml ()
+  (progn
+    (add-to-list 'auto-mode-alist
+		 '("\\.yml\\'" . lang/yaml-mode))
+    t))
+
+(autoload-r! yaml-mode
+	     (yaml-mode)
+	     "yaml-mode"
+	     t)
+
+(rmode! lang/yaml-mode
+	"Emacs mode for yaml"
+	(yaml-mode)
+	config-yaml
+	yaml-mode)
+
