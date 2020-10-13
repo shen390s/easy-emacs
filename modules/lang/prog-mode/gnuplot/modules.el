d951973648a2bedaa1732f6a85c98502d13f3c0e
Rongsong Shen <shenrs@cn.ibm.com>
2020-10-13 17:43:27 +0800

    gnuplot-mode

---
+++ modules/lang/prog-mode/gnuplot/modules.el
@@ -0,0 +1,26 @@
+(scope! gnuplot prog-lang)
+
+(package! :name gnuplot-mode
+	  :docstring "A major Emacs mode for edit gnuplot document"
+	  :pkginfo (gnuplot-mode :type git
+				 :host github
+				 :repo "emacsorphanage/gnuplot"))
+
+(defun config-gnuplot ()
+  (DEBUG! "configuring gnuplot mode...")
+  (progn
+    (add-to-list 'auto-mode-alist
+		 '("\\.gp\\'" . lang/gnuplot-mode))
+    t))
+
+(autoload-r! gnuplot-mode
+	     (gnuplot-mode)
+	     "gnuplot"
+	     t)
+
+(rmode! lang/gnuplot-mode
+	"Emacs mode for gnuplot"
+	(gnuplot-mode)
+	config-gnuplot
+	gnuplot-mode)
+
