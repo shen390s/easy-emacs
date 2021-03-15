f37d2d50d7033192013eae76ff6f04e88006454a
Rongsong Shen <shenrs@cn.ibm.com>
2021-03-15 10:49:48 +0800

    devdocs

---
+++ modules/app/devdocs/modules.el
@@ -0,0 +1,21 @@
+(package! :name devdocs-lookup
+	  :docstring "Search the word at point with devdocs"
+	  :pkginfo (devdocs-lookup :type git
+				   :host github
+				   :repo "kaushalmodi/devdocs-lookup"))
+
+(defun config-devdocs ()
+  t)
+
+(defun enable-devdocs ()
+    (progn
+      (require 'devdocs-lopokup)
+      (devdocs-setup)
+      (global-set-key "\C-cds" #'devdocs-lookup)))
+
+(feature! devdocs
+	  "Search the word at point with Devdocs"
+	  (devdocs-lookup)
+	  config-devdocs
+	  enable-devdocs
+	  nil)
