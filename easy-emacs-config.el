7b8ae9a217f5dd88322b91a8e6bbfd3a63fe5fc6
Rongsong Shen <shenrs@cn.ibm.com>
2021-05-03 18:05:57 +0800

    hlinum

--- easy-emacs-config.el
+++ easy-emacs-config.el
@@ -6,10 +6,10 @@
        (a . 10)
        (b . 20)
        :modes
+       (prog :features +hlinum)
        (c :suffix .c .cc .cpp .c++ .h .hpp
-	  :features +lsp -flymake +hlinum)
-       (lisp :suffix .cl .el .lisp
-	     :features +hlinum)
+	  :features +lsp -flymake -hlinum)
+       (lisp :suffix .cl .el .lisp)
        :ui
        (theme my-theme)
        (font font1)
--- modules/misc/hlinum/modules.el
+++ modules/misc/hlinum/modules.el
@@ -5,11 +5,17 @@
 				:repo "tom-tan/hlinum-mode"))
 
 (defun activate-hlinum (scope &optional phase options)
+  (DEBUG! "activate-hlinum scope %s phase %s options %s"
+	  scope phase options)
   (require 'hlinum)
-  (hlinum-activate)
-  (DEBUG! "activate hlinum for buffer %s"
-	  (buffer-name))
-  (linum-mode 1))
+  (let ((status (plist-get options :status)))
+    (if (> status 0)
+	(progn
+	  (hlinum-activate)
+	  (linum-mode 1))
+      (progn
+	(hlinum-deactivate)
+	(linum-mode -1)))))
 
 (feature-ex! hlinum
 	     "This library extends linum-mode to highlight current line number."
