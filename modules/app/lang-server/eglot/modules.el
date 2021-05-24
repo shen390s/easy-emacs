c0d4f7d2ee2450d9d60607890332eccf3f9564de
Rongsong Shen <shenrs@cn.ibm.com>
2021-05-24 17:53:02 +0800

    eglot

--- modules/app/lang-server/eglot/modules.el
+++ modules/app/lang-server/eglot/modules.el
@@ -5,17 +5,29 @@
 		    :repo "joaotavora/eglot"))
 
 (defun activate-eglot (scope &optional phase options)
+  (require 'project)
   (require 'eglot)
 
+  (unless (fboundp 'project-root)
+    (defun project-root (project)
+      (car (project-roots project))))
+  
   (let ((status (plist-get options :status)))
     (if (and status
 	     (>= status 0))
-	(eglot-ensure)
+	(pcase scope
+	  ('modes (progn
+		    (when (feature-off :flymake options)
+		      (setq eglot-disable-flymake t))
+		    (when (feature-off :eldoc options)
+		      (setq eglot-disable-eldoc t))
+		    (eglot-ensure)))
+	  (_ t))
       t)))
 
 (feature-ex! eglot
 	     "A client for Language Server Protocol server"
-	     (eglot)
+	     (project eglot)
 	     nil
 	     nil ;; eglot will be enabled indirectly by cquery/ccls
 	     activate-eglot)
